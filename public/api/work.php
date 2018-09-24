c1b4116605f20637bc6c42ae3b4a4d730c631623
mdhillma <42544831+mdhillma@users.noreply.github.com>
2018-09-24 12:53:37 -0400

    adding php files and folders to repo

--- public/api/work.php
+++ public/api/work.php
@@ -1,58 +1,13 @@
-<?php //must use ; in PHP php.net for document resource 
+<?php
 
-echo '<pre>'; //to prefix browser to read code as written
-echo "foo\n"; // \n is new line
-echo 'bar\n';
+require '../../app/common.php'
 
-$name = 'Phil'; // $ before variables
+$taskId = intval($_GET['taskId'] ?? 0);
 
-echo "\n";
-echo "My name is {$name}\n";
-echo "My name is" .$name."\n"; // . is concatenate
-
-class Animal
-{
-  public $type = 'Pig';
-}
-$wilbur = new Animal();
-echo $wilbur->type; //-> to dereference instance variables
-//errors will not show to User
-
-if(true){
-  //This
-} else {
-  //That
+if ($task <1) {
+  throw new Exception('Invalid Task ID');
 }
 
-for($i = 0; $i<10; $i++){
-  //stuff
-}
-
-$id=0;
-if(isser($GET['id'])){
-  $id = $_GET['id'];
-}
-
-$id = isset($_GET['id']) ? $_GET['id'] : 0; //same as lines 31-34
-
-$id = $_GET['id'] ?? 0; //coalesce operator ; same as line 38
-
-
-$arr = [
-  'first' => 'Michaela' // => is assignment operator
-  'last' => 'Hillman'
-];
-
-echo '[0]:'.$arr1[0]; //won't work
-echo '[first]:'.$arr1['first']; //will work!
-
-$arr2 = ['one', 'two', 'three'];
-//on the backend, this is happening:
-$arr2 = [
-  0=> 'one',
-  1=> 'two',
-  2=> 'three'];
-
-foreach($arr2 as $key,$val in $arr2){
-  echo $key.':'.$val;
-}
+// 1. Go to the database and get all work associalrd with the $taskId
+// 2. Convert to JsonSerializable
+// 3. Print 
