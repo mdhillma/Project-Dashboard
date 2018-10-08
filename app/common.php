002720fc7134b23f0402e386784a4b5ad9188843
mdhillma <42544831+mdhillma@users.noreply.github.com>
2018-10-07 23:40:37 -0400

    make working directory to common.php

--- app/common.php
+++ app/common.php
@@ -6,4 +6,8 @@ require 'environment.php';
 
 if ($_SERVER ['REQUEST-METHOD'] == 'POST') && stripos($_SERVER['CONTENT_TYPE'], 'application/json') !== false) {$_POST = json_decode(file_get_contents('php://input'), true);}
 
+//change working directory to this file
+chdir(_DIR_);
+set_include_path (_DIR_);
+
 require 'models/Work.php';
--- app/models/Work.php
+++ app/models/Work.php
@@ -12,6 +12,15 @@ class Worker
       $this->start_date = $row['start_date'];
       $this->end_date = $row['end_date'];
 
-      $this->hours = 0;
+      //TODO: Where should this be calculates? $this->hours = 0;
+  }
+
+  public static function getWorkByTask(int $taskId) {
+    // 1. Connect to the database
+    // 2. Run query
+    // 3. Read the results
+    // 4. For each row, make new work object
+    // 5. Return array of work objects
+    return [];
   }
 }
